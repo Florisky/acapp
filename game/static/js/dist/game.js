92f39702ab95935842fd6d1e2c41c8acb3bb68d3
Florisky <1176321468@qq.com>
2023-02-23 22:09:02 +0800

    complete a simple game engine

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -54,38 +54,97 @@ class AcGameMenu{
     }
 }
 
-class AcGamePlayground{
-    constructor(root){
-        this.root = root;
-        this.$playground = $(`<div>游戏界面</div>`);
+let AC_GAME_OBJECTS = [];
 
-        this.hide();
+
+class AcGameObject {
+    constructor() {
+        AC_GAME_OBJECTS.push(this);
+        this.has_called_start = false; // 是否执行过start函数
+        this.timedelta = 0 // 当前帧距离上一帧的时间间隔 单位:ms
+    }
+    // 只会在第一帧执行一次
+    start() {
+
+    }
+    // 每一帧均会执行一次
+    update() {
+        
+    }
+    // 在被销毁前执行一次
+    on_destroy() {
+
+    }
+    // 删除该物体
+    destroy() {
+        this.on_destroy();
+
+        for(let i = 0; i < AC_GAME_OBJECTS.length; i ++) {
+            if (AC_GAME_OBJECTS[i] === this) {
+                AC_GAME_OBJECTS.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
+
+
+let last_timestamp;
+let AC_GAME_ANIMATION = function(timestamp) {
+    for (let i = 0; i < AC_GAME_OBJECTS.length; i ++) {
+        let obj = AC_GAME_OBJECTS[i];
+        if (obj.has_called_start) {
+            obj.start();
+            obj.has_called_start = true;
+        }else {
+            obj.timedelta = timestamp - last_timestamp;
+            obj.update();
+        }
+    }
+
+    last_timestamp = timestamp;
+
+    requestAnimationFrame(AC_GAME_ANIMATION);
+}
+
+requestAnimationFrame(AC_GAME_ANIMATION);
+class AcGamePlayground {
+    constructor(root) {
+        this.root = root;
+        this.$playground = $(`
+            <div class="ac_game_playground"></div>
+        `);
+
+        // this.hide();
 
         this.root.$ac_game.append(this.$playground);
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+
         this.start();
     }
 
-    start(){
+    start() {
 
     }
 
-    update(){
+    update() {
 
     }
 
-    show(){ // 显示playground界面
+    show() { // 显示playground界面
         this.$playground.show();
     }
 
-    hide(){ // 隐藏playground界面
+    hide() { // 隐藏playground界面
         this.$playground.hide();
     }
 }
-class AcGame{
+export class AcGame{
     constructor(id){
         this.id = id;
         this.$ac_game = $('#' + id);
-        this.menu = new AcGameMenu(this);
+       // this.menu = new AcGameMenu(this);
         this.playground = new AcGamePlayground(this);
 
         this.start();
--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -1,27 +1,32 @@
-class AcGamePlayground{
-    constructor(root){
+class AcGamePlayground {
+    constructor(root) {
         this.root = root;
-        this.$playground = $(`<div>游戏界面</div>`);
+        this.$playground = $(`
+            <div class="ac_game_playground"></div>
+        `);
 
-        this.hide();
+        // this.hide();
 
         this.root.$ac_game.append(this.$playground);
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+
         this.start();
     }
 
-    start(){
+    start() {
 
     }
 
-    update(){
+    update() {
 
     }
 
-    show(){ // 显示playground界面
+    show() { // 显示playground界面
         this.$playground.show();
     }
 
-    hide(){ // 隐藏playground界面
+    hide() { // 隐藏playground界面
         this.$playground.hide();
     }
 }
