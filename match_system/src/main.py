deb843c5feb4d641f8a094a7ccdc57d73fe0b006
Florisky <1176321468@qq.com>
2023-11-18 17:31:14 +0800

    add thrift server

---
+++ match_system/src/main.py
@@ -0,0 +1,107 @@
+#! /usr/bin/env python3
+
+import glob
+import sys
+sys.path.insert(0, glob.glob('../../')[0])
+
+
+from match_server.match_service import Match
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+
+from queue import Queue
+from time import sleep
+from threading import Thread
+
+queue = Queue() # 全局消息队列
+
+class Player:
+    def __init__(self, score, uuid, username, profile, channel_name):
+        self.score = score
+        self.uuid = uuid
+        self.username = username
+        self.profile = profile
+        self.channel_name = channel_name
+        self.waiting_time = 0 # 等待时间
+
+class Pool:
+    def __init__(self):
+        self.players = []
+
+    def add_player(self, player):
+        self.players.append(player)
+        print("add player: %s %d" % (player.username, player.score))
+
+    def check_match(self, a, b):
+        dt = abs(a.score - b.score)
+        a_max_dif = a.waiting_time * 50
+        b_max_dif = b.waiting_time * 50
+        return dt <= a_max_dif and dt <= b_max_dif
+
+    def match_success(self, ps):
+        print("Match Success: %s %s %s\n" % (ps[0], ps[1], ps[2]))
+
+    def increase_waiting_time(self):
+        for player in self.players:
+            player.waiting_time += 1
+
+    def match(self):
+        while len(self.players) >= 3:
+            self.players = sorted(self.players, key=lambda p: p.score)
+            flag = False
+            for i in range(len(self.players) - 2):
+                a, b, c = self.players[i], self.players[i + 1], self.players[i + 2]
+                if self.check_match(a, b) and self.check_match(b, c) and self.check_match(a, c):
+                    self.match_success([a, b, c])
+                    self.players = self.players[:i] + self.players[i + 3:]
+                    flag = True
+                    break
+            if not flag:
+                break
+
+        self.increase_waiting_time()
+
+class MatchHandler:
+    def add_player(self, score, uuid, username, profile, channel_name):
+        player = Player(score, uuid, username, profile, channel_name)
+        queue.put(player)
+        return 0
+
+def get_player_from_queue():
+    try:
+        return queue.get_nowait()
+    except:
+        return None
+
+def worker():
+    pool = Pool()
+    while True:
+        player = get_player_from_queue()
+        if player:
+            pool.add_player(player)
+        else:
+            pool.match()
+            sleep(1)
+
+
+if __name__ == '__main__':
+    handler = MatchHandler()
+    processor = Match.Processor(handler)
+    transport = TSocket.TServerSocket(host='127.0.0.1', port=9090)
+    tfactory = TTransport.TBufferedTransportFactory()
+    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+
+    # server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+
+    # You could do one of these for a multithreaded server
+    server = TServer.TThreadedServer(
+        processor, transport, tfactory, pfactory)
+    # server = TServer.TThreadPoolServer(
+    #     processor, transport, tfactory, pfactory)
+    Thread(target=worker, daemon=True).start()
+
+    print('Starting the server...')
+    server.serve()
+    print('done.')
